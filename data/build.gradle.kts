9fba9b64e54cb18dc661bf17ff5d13574b8154e7
mingyuLim <mglim@iportfolio.co.kr>
2024-10-13 18:40:43 +0900

    grable 정리2

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,20 +1,23 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
     kotlin("kapt")
-    id("com.google.dagger.hilt.android")
-    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "com.example.data"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.minSdk.get().toInt()
+
         val authKey = project.findProperty("AUTHORIZATION_KEY") ?: ""
+
         buildConfigField("String", "AUTHORIZATION_KEY", "$authKey")
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -48,20 +51,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-android-compiler:2.48")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
-    implementation("androidx.hilt:hilt-work:1.2.0")
 
-    // 코루틴 추가
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    // hilt
+    implementation(libs.hilt)
+    kapt(libs.hiltKapt)
 
     // Retrofit 및 코루틴 어댑터 추가
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation(libs.bundles.networkLibraries)
 }
 
 kapt {
